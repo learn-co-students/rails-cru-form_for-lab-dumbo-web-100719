5616c9b398b4093eb960d9fafa6af8a6270f9122
Diego Martinez <Diego@Diegos-MacBook-Pro.local>
2019-11-06 16:48:47 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.destroy_all 
+Song.destroy_all 
+Genre.destroy_all 
+
+artist_1 =  Artist.create(name:"Frank Sinatra", bio:"the best singer for New York")
+artist_2 =  Artist.create(name:"John Legend", bio:"best piano player")
+artist_3 =  Artist.create(name:"Mariah Carey", bio:"the diva of all times")
+artist_4 =  Artist.create(name:"Bruno Mars", bio:"best pop")
+
+pop = Genre.create(name:"Pop")
+classic = Genre.create(name:"Classic")
+rb = Genre.create(name:"R&B")
+
+Song.create(name:"Stay the way you are", artist_id: artist_4.id, genre_id: pop.id )
